9dceac2990e603ce678cc1bddf529e02d06fa705
vaidas <vaidas@unity3d.com>
2018-01-22 13:04:41 +0200

    Adding keys.json template, minor bugfix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os.path
 collect_data = False    # True if new data from FogBugz should be collected. Existing data will be used otherwise
 training = True     # True if a new model should be trained. An existing model will be used otherwise
 
-if collect_data or not os.path.isfile('train.txt'):
+if collect_data or not os.path.isfile('train.txt') or not os.path.isfile('test.txt'):
     collector = FBCollector()
     print('Hold on, collecting FogBugz case data...')
     collector.collect('train.txt', (800000, 830000))
