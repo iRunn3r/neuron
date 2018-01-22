3b67a2344f9a5efb29b865d094e0370213488184
iRunn3r <vaidas.budrys3@gmail.com>
2018-01-22 03:00:00 +0200

    Improved usability for main class

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import fasttext
 from fb_scraper import FBCollector
+import os.path
 
-collect_data = True    # True if new data from FogBugz should be collected. Existing data will be used otherwise
+collect_data = False    # True if new data from FogBugz should be collected. Existing data will be used otherwise
 training = True     # True if a new model should be trained. An existing model will be used otherwise
 
-if collect_data:
+if collect_data or not os.path.isfile('train.txt'):
     collector = FBCollector()
+    print('Hold on, collecting FogBugz case data...')
     collector.collect('train.txt', (800000, 830000))
     collector.collect('test.txt', (830000, 831000))
 
-if training:
+if training or not os.path.isfile('model.bin'):
+    print('Training classifier...')
     classifier = fasttext.supervised('train.txt', 'model')
+    print('Done!')
 else:
     classifier = fasttext.load_model('model.bin')
 
 testing = classifier.test('test.txt', 1)
-print(testing.nexamples)
+
+results = '''Test samples: {}
+Precision: {}
+Recall: {}'''
+print(results.format(testing.nexamples, testing.precision, testing.recall))
