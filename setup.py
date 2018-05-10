c52020db32714be0592dd1757b73f0081f14425c
Vaidas Budrys <vaidas.budrys3@gmail.com>
2018-05-10 14:02:11 +0300

    Changed license format in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email='vaidas.budrys3@gmail.com',
     version='v0.1',
     packages=['neuron'],
-    license='mit',
+    license='MIT',
     url='https://bitbucket.org/iRunn3r/neuron/',
     description='Package for predicting FogBugz user report submission quality.',
     long_description=open('README.txt').read()
