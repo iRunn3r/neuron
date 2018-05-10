c4de8b826617df5c79e65828aa7499d47eec377d
Vaidas Budrys <vaidas.budrys3@gmail.com>
2018-05-10 10:03:50 +0300

    Restructured project, added license

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='neuron',
+    author='Vaidas Budrys',
+    author_email='vaidas.budrys3@gmail.com',
+    version='v0.1',
+    packages=['neuron'],
+    license='mit',
+    url='https://bitbucket.org/iRunn3r/neuron/',
+    description='Package for predicting FogBugz user report submission quality.',
+    long_description=open('README.txt').read()
+)
